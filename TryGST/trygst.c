0f642821d198e82f1497fa92f12a7321c102ae92
xiaobaigou1000 <xiaobaigou2000@qq.com>
2022-08-26 16:48:14 +0800

    format code

--- TryGST/trygst.c
+++ TryGST/trygst.c
@@ -1,28 +1,30 @@
-#include<Uefi.h>
-#include<Library/UefiApplicationEntryPoint.h>
-#include<Library/UefiLib.h>
-#include<Library/UefiBootServicesTableLib.h>
+#include <Uefi.h>
+#include <Library/UefiApplicationEntryPoint.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
 
 EFI_STATUS
 EFIAPI
-UefiMain (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
-  )
+UefiMain(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable)
 {
-  while(TRUE){
+  while (TRUE)
+  {
     UINTN index;
-    SystemTable->BootServices->WaitForEvent(1,&SystemTable->ConIn->WaitForKey,&index);
+    SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &index);
     EFI_INPUT_KEY key;
-    EFI_STATUS status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn,&key);
-    if(EFI_ERROR(status)){
+    EFI_STATUS status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
+    if (EFI_ERROR(status))
+    {
       Print(L"Error reading key\n");
     }
-    CHAR16 keyPressed[3]={0};
-    keyPressed[1]='\n';
-    keyPressed[0]=key.UnicodeChar;
+    CHAR16 keyPressed[3] = {0};
+    keyPressed[1] = '\n';
+    keyPressed[0] = key.UnicodeChar;
     Print(keyPressed);
-    if(key.ScanCode == 0x0017){
+    if (key.ScanCode == 0x0017)
+    {
       break;
     }
   }
