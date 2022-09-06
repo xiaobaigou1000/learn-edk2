0812f600df720b9efa732df114dda94d877c2d14
xiaobaigou1000 <xiaobaigou2000@qq.com>
2022-09-06 08:58:20 +0800

    try event notify workaround

--- Chapter06/TryEventNotify/tryeventnotify.c
+++ Chapter06/TryEventNotify/tryeventnotify.c
@@ -3,23 +3,25 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
-void MyEventNotify(IN EFI_EVENT event, IN void *context)
+EFI_EVENT helloEvent = NULL;
+
+VOID MyEventNotify(IN EFI_EVENT event, IN VOID *context)
 {
     static UINTN count = 0;
-    Print(L"My event notify times: %d", count);
+    Print(L"My event notify times: %d\n", count);
     ++count;
     if (count > 5)
     {
-        gBS->SignalEvent(event);
+        gBS->SignalEvent(helloEvent);
     }
 }
 
 EFI_STATUS TestNotify()
 {
-    EFI_EVENT event = NULL;
-    gBS->CreateEvent(EVT_NOTIFY_WAIT, TPL_NOTIFY, (EFI_EVENT_NOTIFY)MyEventNotify, NULL, &event);
+    
+    gBS->CreateEvent(EVT_NOTIFY_WAIT, TPL_NOTIFY, (EFI_EVENT_NOTIFY)MyEventNotify, NULL, &helloEvent);
     UINTN index = 0;
-    gBS->WaitForEvent(1, &event, &index);
+    gBS->WaitForEvent(1, &helloEvent, &index);
     return EFI_SUCCESS;
 }
 
