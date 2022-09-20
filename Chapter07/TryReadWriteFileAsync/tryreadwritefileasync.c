d920661703f264bb1e955f5009201018aa7a3f20
xiaobaigou1000 <xiaobaigou2000@qq.com>
2022-09-20 16:10:51 +0800

    Try read write file async

--- Chapter07/TryReadWriteFileAsync/tryreadwritefileasync.c
+++ Chapter07/TryReadWriteFileAsync/tryreadwritefileasync.c
@@ -9,12 +9,11 @@
 EFI_STATUS EFIAPI OpenAsyncFile(EFI_FILE_PROTOCOL *root, CHAR16 *fileName, UINT64 openMode, OUT EFI_FILE_PROTOCOL **file)
 {
     EFI_FILE_IO_TOKEN openToken;
-    EFI_FILE_PROTOCOL *asyncFile;
     EFI_STATUS status = gBS->CreateEvent(0, TPL_NOTIFY, NULL, NULL, &openToken.Event);
-    status = root->OpenEx(root, &asyncFile, L"async_file.txt", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, 0, &openToken);
+    status = root->OpenEx(root, file, fileName, openMode, 0, &openToken);
     if (EFI_ERROR(status))
     {
-        Print(L"Damn!\n");
+        Print(L"Dispatch Open file failed!\n");
         gBS->CloseEvent(openToken.Event);
         return status;
     }
@@ -23,18 +22,59 @@ EFI_STATUS EFIAPI OpenAsyncFile(EFI_FILE_PROTOCOL *root, CHAR16 *fileName, UINT6
     gBS->WaitForEvent(1, &openToken.Event, &index);
     Print(L"Aync Open Status: %d\n", openToken.Status);
     gBS->CloseEvent(openToken.Event);
-    *file = asyncFile;
     return status;
 }
 
 EFI_STATUS EFIAPI WriteFileWithWaitForEvent(EFI_FILE_PROTOCOL *root)
 {
-    return EFI_UNSUPPORTED;
+    EFI_FILE_PROTOCOL *file;
+    OpenAsyncFile(root, L"async_file.txt", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, &file);
+    EFI_FILE_IO_TOKEN writeToken;
+    gBS->CreateEvent(0, TPL_NOTIFY, NULL, NULL, &writeToken.Event);
+    CHAR16 *textToWrite = L"Hello Async File IO\n";
+    writeToken.Buffer = (void *)textToWrite;
+    writeToken.BufferSize = StrLen(textToWrite) * 2;
+    EFI_STATUS status = file->WriteEx(file, &writeToken);
+    if (EFI_ERROR(status))
+    {
+        gBS->CloseEvent(writeToken.Event);
+        Print(L"Async write dispatch failed. Status Code: %r\n", status);
+        return status;
+    }
+    UINTN index = 0;
+    gBS->WaitForEvent(1, &writeToken.Event, &index);
+    Print(L"Async write status: %d\n", writeToken.Status);
+    gBS->CloseEvent(writeToken.Event);
+    file->Close(file);
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI ReadFileNofiticationFunc(EFI_EVENT event, void *context)
+{
+    EFI_FILE_IO_TOKEN *readToken = (EFI_FILE_IO_TOKEN *)context;
+    Print(L"Async Read Status: %d\nBuffer size: %d\n", readToken->Status, readToken->BufferSize);
+    Print(L"Text in file:\n...File Start...\n");
+    Print(readToken->Buffer);
+    Print(L"\n ...File end...\n");
+    return EFI_SUCCESS;
 }
 
 EFI_STATUS EFIAPI ReadFileWithNotification(EFI_FILE_PROTOCOL *root)
 {
-    return EFI_UNSUPPORTED;
+    EFI_FILE_PROTOCOL *file;
+    OpenAsyncFile(root, L"async_file.txt", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, &file);
+
+    EFI_FILE_IO_TOKEN readToken;
+    gBS->CreateEvent(EVT_NOTIFY_SIGNAL, TPL_NOTIFY, (EFI_EVENT_NOTIFY)ReadFileNofiticationFunc, (void *)&readToken, &readToken.Event);
+    readToken.BufferSize = 1024;
+    gBS->AllocatePool(EfiBootServicesData, readToken.BufferSize, &readToken.Buffer);
+    file->ReadEx(file, &readToken);
+    UINTN index = 0;
+    gBS->WaitForEvent(1, &readToken.Event, &index);
+    gBS->FreePool(readToken.Buffer);
+    gBS->CloseEvent(readToken.Event);
+
+    return EFI_SUCCESS;
 }
 
 EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
@@ -51,6 +91,7 @@ EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Syste
     status = fileSystem->OpenVolume(fileSystem, &root);
 
     WriteFileWithWaitForEvent(root);
+    ReadFileWithNotification(root);
 
     return EFI_SUCCESS;
 }
