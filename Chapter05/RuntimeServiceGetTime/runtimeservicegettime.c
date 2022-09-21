568d446bc50cf02f6b3db0a39586bd4ed24e144b
xiaobaigou1000 <xiaobaigou2000@qq.com>
2022-09-21 16:07:13 +0800

    fix status unused warning

--- Chapter05/RuntimeServiceGetTime/runtimeservicegettime.c
+++ Chapter05/RuntimeServiceGetTime/runtimeservicegettime.c
@@ -4,12 +4,14 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/DebugLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
+#include <Library/DebugLib.h>
 
 EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
 {
     EFI_TIME time;
-    EFI_STATUS status = gRT->GetTime(&time, NULL);
-    ASSERT(status == EFI_SUCCESS);
+    EFI_STATUS status;
+    status = gRT->GetTime(&time, NULL);
+    ASSERT_EFI_ERROR(status);
     Print(L"Get boot time:\n");
     Print(L"%d.%d.%d %d:%d:%d \n", time.Year, time.Month, time.Day, time.Hour, time.Minute, time.Second);
     return EFI_SUCCESS;
