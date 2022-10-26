b61f646fe2274b5e90189ba7f8858a331541d892
xiaobaigou1000 <xiaobaigou2000@qq.com>
2022-10-26 16:55:54 +0800

    try display image(unfinished)

--- TryUsingLibStd/tryusinglibstd.c
+++ TryUsingLibStd/tryusinglibstd.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
 #include <Guid/FileInfo.h>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -11,7 +13,6 @@
 
 int main(int argc, char **argv)
 {
-    printf("Hello world edk2-libc\n");
 
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *simpleFileSystemProtocol = NULL;
     gBS->LocateProtocol(&gEfiSimpleFileSystemProtocolGuid, NULL, (void **)&simpleFileSystemProtocol);
@@ -24,14 +25,50 @@ int main(int argc, char **argv)
 
     EFI_FILE_INFO *fileInfo = NULL;
     UINTN efiFileInfoSize = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 512;
-    gBS->AllocatePool(EfiBootServicesData, efiFileInfoSize, (void **)fileInfo);
-    file->GetInfo(file, &gEfiFileInfoGuid, efiFileInfoSize, &fileInfo);
-    
+    gBS->AllocatePool(EfiBootServicesData, efiFileInfoSize, (void **)&fileInfo);
+    file->GetInfo(file, &gEfiFileInfoGuid, &efiFileInfoSize, fileInfo);
+    Print(L"File Name: %s, File Size: %d\n", fileInfo->FileName, fileInfo->FileSize);
+    DebugPrint(DEBUG_VERBOSE, "Read File Info\n");
+
+    void *fileBuffer = NULL;
+    gBS->AllocatePool(EfiBootServicesData, fileInfo->FileSize, &fileBuffer);
+
+    file->Read(file, &fileInfo->FileSize, fileBuffer);
 
     int width = 0;
     int height = 0;
     int original_channels = 0;
     int desired_channels = 4;
+    stbi_uc *image = stbi_load_from_memory((stbi_uc *)fileBuffer, fileInfo->FileSize, &width, &height, &original_channels, desired_channels);
+    printf("Image Read Complete Status: %d\n", image != NULL);
+    gBS->FreePool(fileInfo);
+
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *pixel = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)image;
+    for (UINTN i = 0; i < width * height; i++)
+    {
+        EFI_GRAPHICS_OUTPUT_BLT_PIXEL *current = pixel + i;
+        UINT8 temp = current->Blue;
+        current->Blue = current->Red;
+        current->Red = temp;
+    }
+
+    // EFI_GRAPHICS_OUTPUT_BLT_PIXEL singleColor;
+    // singleColor.Blue=0xff;
+    // singleColor.Green=0;
+    // singleColor.Red=0;
+    // singleColor.Reserved=0;
+
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+    
+    gBS->LocateProtocol(&gEfiGraphicsOutputProtocolGuid, NULL, (void **)&gop);
+    gop->SetMode(gop, 0);
+    gop->Blt(gop, pixel, EfiBltVideoFill, 0, 0, 0, 0, 100, 100, 0);
+
+    UINTN index = 0;
+    gBS->WaitForEvent(1, &gST->ConIn->WaitForKey, &index);
+
+    stbi_image_free(image);
+    gBS->FreePool(fileBuffer);
 
     return EFI_SUCCESS;
 }
